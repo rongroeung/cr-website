c1b396724e2ad4365d7bb51ac619a2fad8d379c0
Kosalvireak <virakvary@gmail.com>
2024-08-10 23:13:15 +0700

    globe logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -34,7 +34,7 @@
       </div>
 
       <div class="dropdown-menu">
-        <a href="#"
+        <a href="/about" @click="handleNavbarClick('about')"
           >About<svg
             xmlns="http://www.w3.org/2000/svg"
             height="32px"
@@ -45,13 +45,13 @@
             <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" /></svg
         ></a>
         <div class="sub-menu">
-          <a href="#">Vision</a>
-          <a href="#">Team</a>
-          <a href="#">Milestone</a>
+          <a href="/church-board">Church Board</a>
+          <a href="/pastoral-team">Pastoral Team</a>
+          <a href="/milestone">Milestone</a>
         </div>
       </div>
       <div class="dropdown-menu">
-        <a href="#"
+        <a href="/ministries"
           >Ministries<svg
             xmlns="http://www.w3.org/2000/svg"
             height="32px"
@@ -62,13 +62,13 @@
             <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" /></svg
         ></a>
         <div class="sub-menu">
-          <a href="#">Campus Ministry</a>
-          <a href="#">Worship Ministry</a>
-          <a href="#">Sport Ministry</a>
+          <a href="/campus-ministry">Campus Ministry</a>
+          <a href="/worship-ministry">Worship Ministry</a>
+          <a href="/sport-ministry">Sport Ministry</a>
         </div>
       </div>
       <div class="dropdown-menu">
-        <a href="#"
+        <a href="/mission"
           >Mission<svg
             xmlns="http://www.w3.org/2000/svg"
             height="32px"
@@ -79,14 +79,14 @@
             <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" /></svg
         ></a>
         <div class="sub-menu">
-          <a href="#">Church Outreach</a>
-          <a href="#">Micro-Enterprise Projects</a>
-          <a href="#">Certificate In Ministry Leadership</a>
-          <a href="#">Sponsor A Child</a>
+          <a href="/church-outreach">Church Outreach</a>
+          <a href="/micro-enterprise-project">Micro-Enterprise Projects</a>
+          <a href="/certificate-in-ministry-leadership">Certificate In Ministry Leadership</a>
+          <a href="/sponsor-a-chile">Sponsor A Child</a>
         </div>
       </div>
       <div class="dropdown-menu">
-        <a href="#"
+        <a href="/get-involve"
           >Get Involved<svg
             xmlns="http://www.w3.org/2000/svg"
             height="32px"
@@ -97,14 +97,14 @@
             <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" /></svg
         ></a>
         <div class="sub-menu">
-          <a href="#">Short-Term Missions</a>
-          <a href="#">Professional Equipper</a>
-          <a href="#">Volunteer</a>
-          <a href="#">Give</a>
+          <a href="/short-term-missions">Short-Term Missions</a>
+          <a href="/professional-equipper">Professional Equipper</a>
+          <a href="/volunteer">Volunteer</a>
+          <a href="/give">Give</a>
         </div>
       </div>
       <div class="dropdown-menu">
-        <a href="#"
+        <a href="/news"
           >News<svg
             xmlns="http://www.w3.org/2000/svg"
             height="32px"
@@ -115,12 +115,12 @@
             <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" /></svg
         ></a>
         <div class="sub-menu">
-          <a href="#">Sunday Sermons</a>
-          <a href="#">Events</a>
-          <a href="#">Church News</a>
+          <a href="/sunday-sermons">Sunday Sermons</a>
+          <a href="/events">Events</a>
+          <a href="/church-news">Church News</a>
         </div>
       </div>
-      <div class="dropdown-menu"><a href="#">Contact</a></div>
+      <div class="dropdown-menu"><a href="/contact">Contact</a></div>
 
       <div class="navbar-search-icon-desktop">
         <svg
@@ -136,13 +136,31 @@
           ></path>
         </svg>
       </div>
+      <div class="navbar-language-icon-desktop cursor-pointer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="#e8eaed"
+        >
+          <path
+            d="M480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-155.5t86-127Q252-817 325-848.5T480-880q83 0 155.5 31.5t127 86q54.5 54.5 86 127T880-480q0 82-31.5 155t-86 127.5q-54.5 54.5-127 86T480-80Zm0-82q26-36 45-75t31-83H404q12 44 31 83t45 75Zm-104-16q-18-33-31.5-68.5T322-320H204q29 50 72.5 87t99.5 55Zm208 0q56-18 99.5-55t72.5-87H638q-9 38-22.5 73.5T584-178ZM170-400h136q-3-20-4.5-39.5T300-480q0-21 1.5-40.5T306-560H170q-5 20-7.5 39.5T160-480q0 21 2.5 40.5T170-400Zm216 0h188q3-20 4.5-39.5T580-480q0-21-1.5-40.5T574-560H386q-3 20-4.5 39.5T380-480q0 21 1.5 40.5T386-400Zm268 0h136q5-20 7.5-39.5T800-480q0-21-2.5-40.5T790-560H654q3 20 4.5 39.5T660-480q0 21-1.5 40.5T654-400Zm-16-240h118q-29-50-72.5-87T584-782q18 33 31.5 68.5T638-640Zm-234 0h152q-12-44-31-83t-45-75q-26 36-45 75t-31 83Zm-200 0h118q9-38 22.5-73.5T376-782q-56 18-99.5 55T204-640Z"
+          />
+        </svg>
+      </div>
     </div>
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'Navbar'
+  name: 'Navbar',
+  methods: {
+    handleNavbarClick(args) {
+      console.log(args)
+    }
+  }
 }
 </script>
 
@@ -190,7 +208,7 @@ html {
 .navbar-logo {
   display: flex;
   justify-content: center;
-  margin-right: 100px;
+  margin-right: 120px;
 }
 
 .navbar-logo img {
@@ -203,7 +221,7 @@ html {
 .navbar-search-icon-desktop {
   display: flex;
   justify-content: center;
-  margin-left: 100px;
+  margin-left: 120px;
   cursor: pointer;
 }
 
@@ -260,7 +278,7 @@ nav svg {
   background-size: cover;
 }
 
-@media (max-width: 1100px) {
+@media (max-width: 1200px) {
   nav {
     justify-content: flex-end;
   }
@@ -273,7 +291,7 @@ nav svg {
     top: 0;
     right: -100%;
     z-index: 10;
-    width: 300px;
+    width: 400px;
     background-color: var(--secondary-color);
     box-shadow: -5px 0 5px rgba(0, 0, 0, 0.25);
     transition: 0.75s ease-out;
@@ -287,6 +305,9 @@ nav svg {
   }
 
   .navbar-search-icon-desktop {
+    display: none;
+  }
+  .navbar-language-icon-desktop {
     display: none;
   }
 
@@ -344,6 +365,12 @@ nav svg {
     top: 0;
     left: 0;
     z-index: 9;
+  }
+}
+
+@media (max-width: 500px) {
+  .links-container {
+    width: 100%;
   }
 }
 </style>
