1d672130b41b9d9c5a43827fda383217609c718f
Kosal Vireak <kosalvireak.khoeun@khalibre.com>
2024-08-20 13:14:52 +0700

    feat: church board image

--- src/views/ChurchBoardPage.vue
+++ src/views/ChurchBoardPage.vue
@@ -3,33 +3,71 @@ export default {
   name: 'ChurchBoardPage',
   data() {
     return {
-      Principles: [
-        {
-          header: 'Purposeful Service',
-          content:
-            'As stewards of our churchS vision and mission, your role is pivotal. May your decisions be bathed in prayer, guided by GodS will, and aimed at serving our congregation with purpose.'
-        },
-        {
-          header: 'Unity and Harmony',
-          content:
-            'Let us embrace our diversity as a strength. Working together in unity, respecting each others perspectives, we enrich our discussions and fortify our ministry.'
-        },
-        {
-          header: 'Servant Leadership',
-          content:
-            ' Following the example of Jesus. true leadership lies in selfless service. May your leadership be marked by humility, compassion, and a heart devoted to others.'
-        },
-        {
-          header: 'Prayerful Dependence',
-          content:
-            'Seek God"s wisdom in every decision. May our meetings be sanctified by prayer, inviting His guidance for the growth and flourishing of our church.'
-        },
-        {
-          header: 'Encouragement and Support',
-          content:
-            'Lean on one another. Encourage, uplift, and support fellow board members. Together, we form a team fueled by love for Christ and His people.'
-        }
-      ]
+      ChurchBoard: {
+        title: 'Church Board of Directors 2024-2025',
+        description:
+          'We come together as a community, bound by faith and purpose, to share in the leadership and stewardship of our beloved church. With hearts full of gratitude, we thank God for calling us, the alumni, to join hands and build up the body of Christ.',
+        image: `${this.$assetsPath}/img/Group-Photo---Church-Board-Members-2024.jpg`,
+        imageDescription:
+          'In the photo (L to R): Mrs. Sam Dalis, Mr. Gnonn Danet, Mrs. Sous Sinoun, Mrs. Chann Socheata, Mr. Norl Chanly, Mr. Theng Rathrongroeung, Pastor Moeun Rathana',
+        Principles: [
+          {
+            header: 'Purposeful Service',
+            content:
+              'As stewards of our churchS vision and mission, your role is pivotal. May your decisions be bathed in prayer, guided by GodS will, and aimed at serving our congregation with purpose.'
+          },
+          {
+            header: 'Unity and Harmony',
+            content:
+              'Let us embrace our diversity as a strength. Working together in unity, respecting each others perspectives, we enrich our discussions and fortify our ministry.'
+          },
+          {
+            header: 'Servant Leadership',
+            content:
+              ' Following the example of Jesus. true leadership lies in selfless service. May your leadership be marked by humility, compassion, and a heart devoted to others.'
+          },
+          {
+            header: 'Prayerful Dependence',
+            content:
+              'Seek God"s wisdom in every decision. May our meetings be sanctified by prayer, inviting His guidance for the growth and flourishing of our church.'
+          },
+          {
+            header: 'Encouragement and Support',
+            content:
+              'Lean on one another. Encourage, uplift, and support fellow board members. Together, we form a team fueled by love for Christ and His people.'
+          }
+        ],
+        BoardMembers: [
+          {
+            name: 'Pastor Moeun Rathana',
+            role: 'Chair Person'
+          },
+          {
+            name: 'Mrs. Sous Sinoun',
+            role: 'Vice Chair Person'
+          },
+          {
+            name: 'Mrs. Sam Dalis',
+            role: 'Secretary'
+          },
+          {
+            name: 'Mrs. Chann Socheata',
+            role: 'Treasurer'
+          },
+          {
+            name: 'Mr. Gnonn Danet',
+            role: 'Member'
+          },
+          {
+            name: 'Mr. Norl Chanly',
+            role: 'Member'
+          },
+          {
+            name: 'Mr. Theng Rathrongroeung',
+            role: 'Member'
+          }
+        ]
+      }
     }
   }
 }
@@ -42,21 +80,34 @@ export default {
     </div>
     <div class="content w-4/5 mx-auto my-20">
       <p class="text-secondary text-2xl md:text-4xl xl:text-5xl text-center mb-2">
-        Church Board of Directors 2024-2025
+        {{ ChurchBoard.title }}
       </p>
       <p class="text-sm md:text-base my-8">
-        We come together as a community, bound by faith and purpose, to share in the leadership and
-        stewardship of our beloved church. With hearts full of gratitude, we thank God for calling
-        us, the alumni, to join hands and build up the body of Christ.
+        {{ ChurchBoard.description }}
       </p>
       <p class="text-sm md:text-base my-8">Guiding Principles for Our Board:</p>
 
       <div class="p-4 pt-0">
         <ul class="list-disc m-2">
-          <template v-for="principle in Principles" :key="principle">
-            <li>
+          <template v-for="principle in ChurchBoard.Principles" :key="principle">
+            <li class="text-sm md:text-base my-1">
               <b>{{ principle.header }}</b
               >: {{ principle.content }}
+            </li>
+          </template>
+        </ul>
+      </div>
+      <div class="flex-center flex-col">
+        <img :src="ChurchBoard.image" alt="" class="w-4/5" />
+        <p class="text-sm md:text-base my-2">
+          {{ ChurchBoard.imageDescription }}
+        </p>
+      </div>
+      <div class="p-4 pt-0">
+        <ul class="list-disc m-2">
+          <template v-for="boardMember in ChurchBoard.BoardMembers" :key="boardMember">
+            <li class="text-sm md:text-base italic my-1">
+              {{ boardMember.name }} | {{ boardMember.role }}
             </li>
           </template>
         </ul>
@@ -65,5 +116,4 @@ export default {
   </section>
 </template>
 
-<script scoped>
-</script>
+<script scoped></script>
