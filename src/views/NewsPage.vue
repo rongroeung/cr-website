5f9b237af7a064e080f41e73b613c1749423dd35
Kosal Vireak <kosalvireak.khoeun@khalibre.com>
2024-09-27 15:59:16 +0700

    feat: news page

--- src/views/NewsPage.vue
+++ src/views/NewsPage.vue
@@ -1,16 +1,49 @@
 <script>
+import PageHeader from '@/components/PageHeader.vue'
 export default {
-  name: 'NewsPage'
+  name: 'NewsPage',
+  components: { PageHeader },
+  data() {
+    return {
+      section1: null
+    }
+  },
+  async created() {
+    this.section1 = await this.getContentById('20001001')
+  }
 }
 </script>
 
 <template>
   <section class="bg-secondary h-fit w-full flex-center flex-col">
-    <div class="image-header h-300-px w-full flex-center">
-      <p class="text-4xl xl:text-5xl" v-t="'news'"></p>
+    <div id="20001001" v-if="section1" class="w-full">
+      <PageHeader :section="section1" />
     </div>
-    <div class="content w-full h-full">
-      <ComingSoon />
+    <div class="bg-cr-gray content w-full h-full">
+      <div class="flex justify-center items-center h-30rem">
+        <div class="max-w-sm rounded-lg border border-gray-200 shadow-lg bg-white">
+          <div class="p-6">
+            <div class="flex items-center">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Telegram_2019_Logo.svg/2048px-Telegram_2019_Logo.svg.png"
+                alt="Telegram"
+                class="w-12 h-12 mr-4"
+              />
+              <div>
+                <h2 class="text-lg font-semibold">Join Our Telegram Channel</h2>
+                <p class="text-gray-600">Stay updated with the latest news and alerts!</p>
+              </div>
+            </div>
+            <a href="https://t.me/crossroadscommunity" target="_blank">
+              <button
+                class="mt-4 w-full px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition ease-in-out"
+              >
+                Join Now
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
