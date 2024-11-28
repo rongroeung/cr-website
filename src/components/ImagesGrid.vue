335f7467fe09118bc6de702afe4dfaf0c43ec933
Kosal Vireak <kosalvireak.khoeun@khalibre.com>
2024-11-28 13:51:04 +0700

    width

--- src/components/ImagesGrid.vue
+++ src/components/ImagesGrid.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-center flex-row flex-wrap">
     <template v-for="(image, index) in images" :key="index">
-      <img :src="image.url" :alt="image.name" class="w-full lg:w-1/2 cr-img-md-3-2 p-2" />
+      <img :src="image.url" :alt="image.name" class="lg:w-1/2 cr-img-md-16-9" />
     </template>
   </div>
 </template>
